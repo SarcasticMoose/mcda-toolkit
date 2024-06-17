e79bd433edbde46bbef7d137a3cb0eed22a8b7ad
SarcasticMoose <kubat260@gmail.com>
2024-06-17 13:52:06 +0200

    feat: added logarithmic normalization

---
+++ src/McdaToolkit/NormalizationMethods/Types/Sum/LogarithmicNormalization.cs
@@ -0,0 +1,20 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using McdaToolkit.NormalizationMethods.Interfaces;
+
+namespace McdaToolkit.NormalizationMethods.Types.Sum;
+
+public class LogarithmicNormalization : INormalize<double>
+{
+    public Vector<double> Normalize(Vector<double> data, bool cost)
+    {
+        var product  = data.Aggregate(1.0,(x,y) => x * y);
+        var exp = data.PointwiseLog() / Math.Log(product);
+        
+        if (cost)
+        {
+            return 1 - exp;
+        }
+
+        return exp;
+    }
+}
