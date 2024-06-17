dab307a982fd1623e9a6185b18851097e207c707
SarcasticMoose <kubat260@gmail.com>
2024-06-17 13:52:06 +0200

    feat: added logarithmic normalization

--- src/McdaToolkit/NormalizationMethods/Interfaces/INormalize.cs
+++ src/McdaToolkit/NormalizationMethods/Interfaces/INormalize.cs
@@ -2,7 +2,14 @@ using MathNet.Numerics.LinearAlgebra;
 
 namespace McdaToolkit.NormalizationMethods.Interfaces;
 
+
 public interface INormalize<T> where T : struct, IEquatable<T>, IFormattable
 {
+    /// <summary>
+    /// Normalize vector of data
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="cost"></param>
+    /// <returns>One-dimensional vector contains normalized values</returns>
     Vector<T> Normalize(Vector<T> data, bool cost);
 }
