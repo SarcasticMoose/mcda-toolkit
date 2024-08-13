86fb7bf4120662cdc8c8c3c3c015fafd7b83e09e
SarcasticMoose <kubat260@gmail.com>
2024-08-13 20:11:32 +0200

    feat: added sum and max normalization refactor: used IDataNormalization instead of it implementation docs: added docs for some functionality

--- src/McdaToolkit/NormalizationMethods/Types/Linear/MinMaxNormalization.cs
+++ src/McdaToolkit/NormalizationMethods/Types/Linear/MinMaxNormalization.cs
@@ -3,17 +3,25 @@ using McdaToolkit.NormalizationMethods.Interfaces;
 
 namespace McdaToolkit.NormalizationMethods.Types.Linear;
 
-public class MinMaxNormalization : INormalize<double>
+internal class MinMaxNormalization : INormalize<double>
 {
-    private const double MaxError = 1.11e-16;
+    private const double MaxTolerance = 1.11e-16;
     
+    /// <summary>
+    /// Create normalized vector using vector normalization method 
+    /// </summary>
+    /// <param name="data">One-dimensional vector of data to normalize</param>
+    /// <param name="cost">Describe type of vector, cost or profit</param>
+    /// <returns>
+    /// Return normalized vector
+    /// </returns>
     public Vector<double> Normalize(Vector<double> data, bool cost = false)
     {
         var max = data.Maximum();
         var min = data.Minimum();
         var difference = max - min;
         
-        if (Math.Abs(difference) < MaxError)
+        if (Math.Abs(difference) < MaxTolerance)
         {
             return Vector<double>.Build.Dense(data.Count, _ => 1);
         }
