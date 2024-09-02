d4e51b4d88fd78dd39d8af40fa5862e687a18ecb
SarcasticMoose <kubat260@gmail.com>
2024-09-02 22:38:03 +0200

    feat: added file writer

---
+++ src/McdaToolkit.FileIO/Writer/FileWriter.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using LightResults;
+
+namespace McdaToolkit.FileIO.Writer
+{
+    public class FileWriter : IFileWriter
+    {
+        private const int BufferSize = 4096;
+    
+        public async Task<Result> WriteFileAsync(byte[] buffer, string filePath,CancellationToken ct = default)
+        { 
+            using (var sourceStream = new FileStream(
+                       filePath, 
+                       FileMode.Create,
+                       FileAccess.Write,
+                       FileShare.None,
+                       bufferSize: BufferSize, 
+                       useAsync: true))
+            {
+                await sourceStream.WriteAsync(buffer,0,buffer.Length,ct).ConfigureAwait(false);
+                return Result.Ok();
+            }
+        }
+    }
+}
---
+++ src/McdaToolkit.FileIO/Writer/IFileWriter.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using LightResults;
+
+namespace McdaToolkit.FileIO.Writer
+{
+    public interface IFileWriter
+    {
+        Task<Result> WriteFileAsync(byte[] buffer, string filePath, CancellationToken ct = default);
+    }
+}
