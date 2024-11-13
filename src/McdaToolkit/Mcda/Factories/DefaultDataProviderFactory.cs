d688dccecc3b09fcbf17ba0cb03fabbb17c6a998
SarcasticMoose <kubat260@gmail.com>
2024-11-13 19:02:25 +0100

    feat!: added a factory for the mcda methods as the only way to create them. test: adjusted test to new format of mcda methods feat!: changed the way to provide data for calculation feat!: introduced new type 'IMcdaScore' that will contain the score of the calculations

---
+++ src/McdaToolkit/Mcda/Factories/DefaultDataProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿using McdaToolkit.Mcda.Providers;
+using McdaToolkit.Mcda.Services;
+using McdaToolkit.Mcda.Services.MatrixChecker;
+
+namespace McdaToolkit.Mcda.Factories;
+
+public static class DefaultDataProviderFactory 
+{
+    public static IDataProvider CreateDataProvider()
+    {
+        MatrixCheckerService matrixCheckerService = new();
+        return new DefaultDataProvider(matrixCheckerService);
+    }
+}
---
+++ src/McdaToolkit/Mcda/Methods/Topsis/TopsisScore.cs
@@ -0,0 +1,9 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using McdaToolkit.Mcda.Methods.Abstraction;
+
+namespace McdaToolkit.Mcda.Methods.Topsis;
+
+public record TopsisScore(Vector<double> Score) : IMcdaScore
+{
+    public Vector<double> Score { get; } = Score;
+}
