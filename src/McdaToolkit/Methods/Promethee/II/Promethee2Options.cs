e5f0d637c4445b4be235a9ffb8347ef7bac3ddb4
SarcasticMoose <kubat260@gmail.com>
2025-05-30 22:58:08 +0200

    feat(Ranking): changed namespaces

--- src/McdaToolkit/Methods/Promethee/II/Promethee2Options.cs
+++ src/McdaToolkit/Methods/Promethee/II/Promethee2Options.cs
@@ -1,11 +1,13 @@
-using McdaToolkit.Methods.Promethee2.PreferenceFunctions.Factory;
+using McdaToolkit.Methods.Promethee.II.PreferenceFunctions.Factory;
 using McdaToolkit.Normalization.Enums;
 using McdaToolkit.Shared.Options;
 
-namespace McdaToolkit.Methods.Promethee2;
+namespace McdaToolkit.Methods.Promethee.II;
 
 public record Promethee2Options : IMcdaMethodOptions
 {
     public NormalizationMethod NormalizationMethod { get; set; } = NormalizationMethod.Vector;
     public PreferenceFunction PreferenceFunction { get; set; } = PreferenceFunction.Fshape;
+
+    public bool UseGaia { get; set; } = false;
 }
