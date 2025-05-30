5597d78dae711f89aee735f66924390aa0c2e89f
SarcasticMoose <kubat260@gmail.com>
2025-05-30 22:57:02 +0200

    feat(Ranking): change visibility of constructor to internal

--- src/McdaToolkit/Shared/Ranking/Ranking.cs
+++ src/McdaToolkit/Shared/Ranking/Ranking.cs
@@ -5,8 +5,13 @@ namespace McdaToolkit.Shared.Ranking;
 public record Ranking<T> : IEnumerable<RankingRow<T>> 
     where T : IComparable<T>
 {
-    public IEnumerable<RankingRow<T>> RankingItems { get; set; }
+    internal Ranking(List<RankingRow<T>> rows)
+    {
+        RankingItems = rows.ToArray();
+    }
     
+    public IReadOnlyCollection<RankingRow<T>> RankingItems { get; }
+
     public IEnumerator<RankingRow<T>> GetEnumerator()
         => RankingItems.GetEnumerator();
 
--- src/McdaToolkit/Shared/Ranking/RankingExtension.cs
+++ src/McdaToolkit/Shared/Ranking/RankingExtension.cs
@@ -5,19 +5,15 @@ internal static class RankingExtension
     public static Ranking<TValue> CreateRanking<TValue>(this IEnumerable<TValue> scores) 
         where TValue : IComparable<TValue>
     {
-        return new Ranking<TValue>
-        {
-            RankingItems = scores
-                .Select((value, index) => new { index, value })
-                .OrderByDescending(x => x.value)
-                .Select((x, index) => new RankingRow<TValue>()
-                {
-                    Rank = index + 1,
-                    Alternative = x.index + 1,
-                    Score = x.value
-                })
-                .OrderBy(x => x.Alternative)
-                .ToList()
-        };
+        return new Ranking<TValue>(scores
+            .Select((value, index) => new { index, value })
+            .OrderByDescending(x => x.value)
+            .Select((x, index) => new RankingRow<TValue>(
+                x.index + 1,
+                index + 1,
+                x.value)
+            )
+            .OrderBy(x => x.Alternative)
+            .ToList());
     }
 }
