48a77c127c87a0178fd67536b25e798020b8326d
jakub <kubat260@gmail.com>
2025-08-24 13:16:47 +0200

    chore(tests): replaced Fluent Assertions with xunit v3 assertions chore(tests): replace xunit 2.x with xunit v3
    refactor(tests): rewrite assertions
    refactor(tests): format test files

--- tests/McdaToolkit.UnitTests/DataProviderBuilderTests.cs
+++ tests/McdaToolkit.UnitTests/DataProviderBuilderTests.cs
@@ -1,4 +1,3 @@
-using FluentAssertions;
 using McdaToolkit.Data.Builders;
 using McdaToolkit.Data.Validation.Abstraction;
 
@@ -11,19 +10,19 @@ public class DataProviderBuilderTests
     {
         var matrix = new[,]
         {
-            {3, 6, 4, 20, 2, 30000 },
-            {4, 4, 6, 15, 2.2, 32000 },
-            {6, 5, 9, 18, 3, 32100 },
-            {5, 6, 3, 23, 2.8, 28000 },
-            {4, 8, 7, 30, 1.5, 29000 },
-            {8, 3, 6, 35, 1.9, 27000 },
-            {7, 2, 5, 33, 1.7, 28500 },
-            {3, 8, 3, 34, 1.6, 30500 },
-            {8, 4, 8, 40, 2.5, 33000 },
-            {9, 3, 7, 34, 2, 29800 }
+            { 3, 6, 4, 20, 2, 30000 },
+            { 4, 4, 6, 15, 2.2, 32000 },
+            { 6, 5, 9, 18, 3, 32100 },
+            { 5, 6, 3, 23, 2.8, 28000 },
+            { 4, 8, 7, 30, 1.5, 29000 },
+            { 8, 3, 6, 35, 1.9, 27000 },
+            { 7, 2, 5, 33, 1.7, 28500 },
+            { 3, 8, 3, 34, 1.6, 30500 },
+            { 8, 4, 8, 40, 2.5, 33000 },
+            { 9, 3, 7, 34, 2, 29800 },
         };
-        double[] weights = [0.1,0.2,0.1,0.2,0.1,0.3];
-        int[] types = [1,1,1,-1,-1,-1];
+        double[] weights = { 0.1, 0.2, 0.1, 0.2, 0.1, 0.3 };
+        int[] types = { 1, 1, 1, -1, -1, -1 };
 
         var data = DataBuilder
             .Create()
@@ -31,41 +30,24 @@ public class DataProviderBuilderTests
             .AddDecisionCriteria(types)
             .AddDecisionMatrix(matrix)
             .Build();
-        
-        data.Should().NotBeNull();
-        data.Weights.Storage.ToArray().Should().BeEquivalentTo(weights);
-        data.Types.Should().BeEquivalentTo(types);
-        data.Matrix.Storage.ToArray().Should().BeEquivalentTo(matrix);
+
+        Assert.NotNull(data);
+        Assert.Equal(weights, data.Weights.Storage.ToArray());
+        Assert.Equal(types, data.Types);
+        Assert.Equal(matrix, data.Matrix.Storage.ToArray());
     }
-    
+
     [Fact]
     public void Build_WithoutDecisionMatrix_ShouldThrowArgumentNullException()
     {
-        var matrix = new[,]
-        {
-            {3, 6, 4, 20, 2, 30000 },
-            {4, 4, 6, 15, 2.2, 32000 },
-            {6, 5, 9, 18, 3, 32100 },
-            {5, 6, 3, 23, 2.8, 28000 },
-            {4, 8, 7, 30, 1.5, 29000 },
-            {8, 3, 6, 35, 1.9, 27000 },
-            {7, 2, 5, 33, 1.7, 28500 },
-            {3, 8, 3, 34, 1.6, 30500 },
-            {8, 4, 8, 40, 2.5, 33000 },
-            {9, 3, 7, 34, 2, 29800 }
-        };
-        double[] weights = [0.1,0.2,0.1,0.2,0.1,0.3];
-        int[] types = [1,1,1,-1,-1,-1];
-        
-        Action act = () =>
-        {
-            DataBuilder
-                .Create()
-                .AddWeights(weights)
-                .AddDecisionCriteria(types)
-                .Build();
-        };
+        double[] weights = { 0.1, 0.2, 0.1, 0.2, 0.1, 0.3 };
+        int[] types = { 1, 1, 1, -1, -1, -1 };
 
-        act.Should().Throw<ValidationException>();
+        var ex = Assert.Throws<ValidationException>(() =>
+        {
+            DataBuilder.Create().AddWeights(weights).AddDecisionCriteria(types).Build();
+        });
+
+        Assert.IsType<ValidationException>(ex);
     }
 }
--- tests/McdaToolkit.UnitTests/TestHelpers.cs
+++ tests/McdaToolkit.UnitTests/TestHelpers.cs
@@ -5,19 +5,12 @@ namespace McdaToolkit.UnitTests;
 
 public abstract class TestHelpers
 {
-    public static bool CheckEquality(Matrix<double> normalizedMatrix,double[][] expected)
-    {
-        return normalizedMatrix
-            .EnumerateRows()
-            .Select((x, i) => IsResultTheSame(x, expected[i]))
-            .All(y => y);
-    }
-    public static bool IsResultTheSame(Vector<double> vector,double[] expected)
+    public static bool IsResultTheSame(Vector<double> vector, double[] expected)
     {
         var epsilon = 1.11e-16;
         var result = vector
             .Select(x => x.Round(8))
-            .Zip(expected, (r,e) => new {result = r, expected = e});
+            .Zip(expected, (r, e) => new { result = r, expected = e });
 
         foreach (var item in result)
         {
